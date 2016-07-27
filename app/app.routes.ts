98fb309db6ddebe9507b5e60ff0fbf63f5e6fe37
Dan Wahlin <dwahlin@xmlforasp.net>
2016-07-26 17:26:49 -0700

    Update const name for routes

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -2,11 +2,11 @@ import { provideRouter, RouterConfig } from '@angular/router';
 
 import { CustomersComponent }     from './customers/customers.component';
 
-const routes: RouterConfig = [
+const APP_ROUTES: RouterConfig = [
   { path: '',  pathMatch:'full', redirectTo: '/customers' },
   { path: 'customers', component: CustomersComponent }
 ];
 
 export const APP_ROUTER_PROVIDERS = [
-  provideRouter(routes)
+  provideRouter(APP_ROUTES)
 ];
